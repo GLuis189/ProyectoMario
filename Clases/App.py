878debd88cfb88927964ab3d30a8e2a4473ee30f
nacho <100471955@alumnos.uc3m.es>
2021-12-02 00:32:39 +0100

    Apretar q para salir

--- Clases/App.py
+++ Clases/App.py
@@ -1,14 +1,16 @@
 import pyxel
-
+import Mario
 class App():
     def __init__(self):
         pyxel.init(256, 160, caption="Mario Bross")
-        pyxel.load("mario_assets.pyxres")
+        #pyxel.load("mario_assets.pyxres")
         pyxel.run(self.update, self.draw)
 
 #Luego crearemos update y draw
     def update(self):
-       pass
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
     def draw(self):
         pass
-
+App()

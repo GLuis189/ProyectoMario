88859c43ab6df3f2dd7eff9a9abca8883eaec096
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2021-12-02 17:11:48 +0100

    Intento de spawnear un bloque

--- Clases/App.py
+++ Clases/App.py
@@ -1,16 +1,20 @@
 import pyxel
+from Clases import Bloques
 import Mario
 class App():
     def __init__(self):
-        pyxel.init(256, 160, caption="Mario Bross")
+        pyxel.init(192, 128, caption="Mario Bross", quit_key=pyxel.KEY_Q, fps=60)
         #pyxel.load("mario_assets.pyxres")
         pyxel.run(self.update, self.draw)
+        self.Bloque_Rompible = Bloque_Rompibles(20, 20)
+
 
 #Luego crearemos update y draw
     def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+        pass
 
     def draw(self):
-        pass
+        pyxel.cls(6)
+        self.Bloque_Rompible(Bloques).draw()
+
 App()
--- Clases/Bloques.py
+++ Clases/Bloques.py
@@ -1,3 +1,4 @@
+import pyxel
 
 
 class Bloque(object):
@@ -9,15 +10,20 @@ class Bloque_Rompibles(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
         super().__init__(posicion_x, posicion_y)
         self.Rompible = True
+    def draw(self):
+        pyxel.blt(self.Posicion_X, self.Posicion_Y, 0, 0, 62, 15, 77)
 
 class Bloque_Irompibles(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
         super().__init__(posicion_x, posicion_y)
         self.Rompible = False
-
+    def draw(self):
+        pass
 class BLoque_Interrogacion(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
         super().__init__(posicion_x, posicion_y)
         self.Activado = False
         self.Contenido = [0,[0,1,2]]
+    def draw(self):
+        pass
 

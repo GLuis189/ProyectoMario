528eed9645ec1cc7c5ac90c81490e0f2e6117aac
nacho <100471955@alumnos.uc3m.es>
2021-12-03 16:26:42 +0100

    He intentao hacer los suelos pero no ha funcionao

--- Clases/App.py
+++ Clases/App.py
@@ -1,12 +1,15 @@
 import pyxel
-from Clases import Bloques
+
 import Mario
+import Nivel as Nv
+
+
 class App():
     def __init__(self):
         pyxel.init(192, 128, caption="Mario Bross", quit_key=pyxel.KEY_Q, fps=60)
-        #pyxel.load("mario_assets.pyxres")
+        pyxel.load("assets/mario_assets.pyxres")
         pyxel.run(self.update, self.draw)
-        self.Bloque_Rompible = Bloque_Rompibles(20, 20)
+        self.__nivel = Nv
 
 
 #Luego crearemos update y draw
@@ -15,6 +18,6 @@ class App():
 
     def draw(self):
         pyxel.cls(6)
-        self.Bloque_Rompible(Bloques).draw()
+
 
 App()
--- Clases/Bloques.py
+++ Clases/Bloques.py
@@ -2,16 +2,25 @@ import pyxel
 
 
 class Bloque(object):
-    def __init__(self, posicion_x: int, posicion_y: int) -> None:
+    def __init__(self, posicion_x: int, posicion_y: int):
         self.Posicion_X = posicion_x
         self.Posicion_Y = posicion_y
+
+
 
 class Bloque_Rompibles(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
         super().__init__(posicion_x, posicion_y)
         self.Rompible = True
+
+
+class Bloque_Suelo(Bloque):
+    def __init__(self, posicion_x, posicion_y):
+        super().__init__(posicion_x, posicion_y)
+
     def draw(self):
-        pyxel.blt(self.Posicion_X, self.Posicion_Y, 0, 0, 62, 15, 77)
+        pyxel.blt(self.Posicion_X, self.Posicion_Y, 0, 0, 62, 15, 15)
+
 
 class Bloque_Irompibles(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
@@ -19,11 +28,13 @@ class Bloque_Irompibles(Bloque):
         self.Rompible = False
     def draw(self):
         pass
+
+
 class BLoque_Interrogacion(Bloque):
     def __init__(self, posicion_x: int, posicion_y: int):
         super().__init__(posicion_x, posicion_y)
         self.Activado = False
-        self.Contenido = [0,[0,1,2]]
+        self.Contenido = [0, [0, 1, 2]]
     def draw(self):
         pass
 

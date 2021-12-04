001146dd4acd217ce7c69e49f5570c961451d861
nacho <100471955@alumnos.uc3m.es>
2021-12-04 18:39:34 +0100

    salto

--- Pruebas_Nacho.py
+++ Pruebas_Nacho.py
@@ -26,8 +26,8 @@ class Mario():
         return self.__vy
 
     def __reset(self):
-        self.__x = 0
-        self.__y = 80
+        self.__x = 20
+        self.__y = 40
         self.__w = 14
         self.__h = 16
         self.__vy = -1
@@ -41,22 +41,45 @@ class Mario():
         # Al pulsar A o <- el mario se mueve a la izq
         if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT):
             self.__x = max(0, self.__x - 2)
-
+            self.__vx -= 1
+        else:
+            self.__vx = 0
         # Al pulsar D o -> el mario se mueve a la derecha hasta la mitad de la pantalla
         # Con self.__x if 128//2 == self.__x - self.__w hago que no se mueva si esta en la mitad
         if pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT):
             self.__x = self.__x if 128//2 == self.__x - self.__w else max(0, self.__x + 2)
+            self.__vx += 1
+        else:
+            self.__vx = 0
+        # Al pulsar el espacio el mario salta
+        if pyxel.btn(pyxel.KEY_SPACE) or pyxel.btn(pyxel.KEY_UP):
+            if self.__y >= 64:
+                self.__vy = 1
+                self.__y -= self.__vy * 6  # la velocidad a la que salta
 
-        self.__y -= self.__vy
+
+
+        if self.__y == 112 - 16:  # con esto el mario deja de tener gravedad a la altura del suelo y asi no sigue bajando hasta la mitad del bloque
+            self.__y = self.__y
+        else:
+            self.__y -= self.__vy
 
 
     def colisionar(self):
-        self.__y -= 16
+
+        #self.__y -= 16
         self.__vy = 0
 
 
     def draw(self):
-        pyxel.blt(self.__x, self.__y, 0, 2, 98, self.__w, self.__h, 12)
+        pyxel.blt(self.__x,
+                  self.__y,
+                  0,
+                  2 if self.__vx == 0 else 18 or 1 if self.__vy > 0 else 2,
+                  79 if self.__vy > 0 else 98,
+                  self.__w if self.__vx >= 0 else self.__w * -1,
+                  self.__h,
+                  12)
 
 class Bloque():
     def __init__(self, x, y):
@@ -65,6 +88,7 @@ class Bloque():
         self.__w = 16
         self.__h = 16
         self.__is_activo = True
+
 
     @property
     def x(self):
@@ -88,7 +112,7 @@ class Bloque():
 
     def update(self, mario: Mario):
         if self.__is_activo:
-            if mario.x == self.__x and mario.y == self.__y:
+            if mario.y == self.__y:
                 mario.colisionar()
 
 

63e7570028684d55487d0b06faabc4656e71ba62
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2021-12-12 14:35:41 +0100

    limpieza mario

--- Clases/Mario.py
+++ Clases/Mario.py
@@ -71,7 +71,6 @@ class Mario():
         self.encimadebloque = False
         self.Supermario = False
         self.__is_alive = True
-        self.__Super_Mario = False
         self.__Mario_Fuego = False
         self.__Ganar = False
 
@@ -93,31 +92,30 @@ class Mario():
         if not self.__Ganar:
             if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT):
                 self.__x = max(0, self.__x - 2)
-                self.__vx = 1
                 if self.__w > 0:
                     self.__w = -self.__w
                 if pyxel.frame_count % 30 < 15 and not pyxel.btn(pyxel.KEY_SPACE):
-                    if self.__vx > 0 and not self.Supermario or not self.__Mario_Fuego:
+                    if not self.Supermario or not self.__Mario_Fuego:
                         self.__q1 = 18
                         self.__q2 = 98
 
-                    if self.__vx > 0 and self.Supermario:
+                    if self.Supermario:
                         self.__q1 = 88
                         self.__q2 = 82
 
-                    if self.__vx > 0 and self.__Mario_Fuego:
+                    if self.__Mario_Fuego:
                         self.__q1 = 39
                         self.__q2 = 135
 
                 else:
-                    if self.__vx > 0 and not self.Supermario or not self.__Mario_Fuego:
+                    if not self.Supermario or not self.__Mario_Fuego:
                         self.__q1 = 0
                         self.__q2 = 98
 
-                    if self.__vx > 0 and self.Supermario:
+                    if self.Supermario:
                         self.__q1 = 105
                         self.__q2 = 82
-                    if self.__vx > 0 and self.__Mario_Fuego:
+                    if self.__Mario_Fuego:
                         self.__q1 = 122
                         self.__q2 = 195
 
@@ -129,52 +127,50 @@ class Mario():
             if pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT):
 
                 self.__x = self.__x if 96 == self.__x - self.__w else min(192 / 2, max(0, self.__x + 2))
-                self.__vx = 1
+
                 if self.__w < 0:
                     self.__w = -self.__w
                 if pyxel.frame_count % 30 < 15 and not pyxel.btn(pyxel.KEY_SPACE):
-                    if self.__vx > 0 and not self.Supermario or not self.__Mario_Fuego:
+                    if not self.Supermario or not self.__Mario_Fuego:
                         self.__q1 = 18
                         self.__q2 = 98
 
-                    if self.__vx > 0 and self.Supermario:
+                    if self.Supermario:
                         self.__q1 = 88
                         self.__q2 = 82
 
-                    if self.__vx > 0 and self.__Mario_Fuego:
+                    if self.__Mario_Fuego:
                         self.__q1 = 39
                         self.__q2 = 135
 
                 else:
-                    if self.__vx > 0 and not self.Supermario or not self.__Mario_Fuego:
+                    if not self.Supermario or not self.__Mario_Fuego:
                         self.__q1 = 0
                         self.__q2 = 98
 
-                    if self.__vx > 0 and self.Supermario:
+                    if self.Supermario:
                         self.__q1 = 106
                         self.__q2 = 82
 
-                    if self.__vx > 0 and self.__Mario_Fuego:
+                    if self.__Mario_Fuego:
                         self.__q1 = 122
                         self.__q2 = 195
 
-            else:
-                self.__vx = 0
             # Al pulsar el espacio el mario salta
             if pyxel.btn(pyxel.KEY_SPACE) or pyxel.btn(pyxel.KEY_UP):
                 #if self.encimadebloque == True:
                     #self.encimadebloque = False
                 self.__vy = 1
                 self.__y -= self.__vy * 5  # la velocidad a la que salta
-                if self.__vy > 0 and not self.Supermario or self.__vy > 0 and not self.__Mario_Fuego:
+                if not self.Supermario or self.__vy > 0 and not self.__Mario_Fuego:
                     self.__q1 = 2
                     self.__q2 = 80
 
-                if self.__vy > 0 and self.Supermario:
+                if self.Supermario:
                     self.__q1 = 146
                     self.__q2 = 80
 
-                if self.__vy > 0 and self.__Mario_Fuego:
+                if self.__Mario_Fuego:
                     self.__q1 = 67
                     self.__q2 = 136
 
